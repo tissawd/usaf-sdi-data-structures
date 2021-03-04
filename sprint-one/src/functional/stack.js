0e1b33cf3424af498c2bd4eb41ebab861b3fa2ca
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-03-04 16:14:10 -0600

    Finished functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,31 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  let currentTop = 0;
+ 
 
   // Implement the methods below
   someInstance.push = function(value) {
+    currentTop += 1;
+    storage[currentTop] = value;
+    
   };
 
   someInstance.pop = function() {
+    if (currentTop > 0){
+    // store value 
+    let valueNext = storage[currentTop];
+    // remove property
+    delete storage[currentTop];
+    // decrement top
+    currentTop -= 1;  
+    //return val
+    return valueNext;
+    }
   };
 
   someInstance.size = function() {
+    return currentTop;
   };
 
   return someInstance;
