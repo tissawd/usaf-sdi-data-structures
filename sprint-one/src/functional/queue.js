dca6455db90ea2ddaa0a3320e6d694cae2373af9
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-03-04 16:46:06 -0600

    Completed Functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,31 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
+  
+  let frontOfLine = 0; 
+  let backOfLine = 0;
+   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    backOfLine ++;
+    storage[backOfLine] = value;
   };
 
   someInstance.dequeue = function() {
+    if (backOfLine > frontOfLine) {
+      frontOfLine ++;
+      let valueFront = storage[frontOfLine];
+      delete storage[frontOfLine];
+      return valueFront;
+    }
   };
 
   someInstance.size = function() {
+    let size = (backOfLine - frontOfLine);
+    return size;
   };
 
   return someInstance;
 };
+
+
