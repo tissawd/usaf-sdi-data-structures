7c2a95c176b59caa44b9c580810d54882b0b3288
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-03-05 14:40:36 -0600

    Completed pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
+  this.currentSize = 0;
 };
+
+Stack.prototype.push = function(value){
+  this.currentSize++;
+  this.storage[this.currentSize] = value;
+
+};
+
+Stack.prototype.pop = function(){
+  if (this.currentSize > 0){
+    let tempVal = this.storage[this.currentSize];
+    delete this.storage[this.currentSize];
+    this.currentSize--;
+    return tempVal;
+  }
+};
+
+Stack.prototype.size = function(){
+  return this.currentSize;
+};
+
+const stack = new Stack();
 
 
