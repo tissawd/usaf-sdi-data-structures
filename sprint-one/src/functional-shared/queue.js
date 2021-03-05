2c21263391e9d1c8c2b8e9cd3202577dd73cbc6e
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-03-05 13:08:26 -0600

    finished functional-shared queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,48 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //declare empty object
+  let queueInstance = {};
+  //declare a storage object
+  queueInstance.storageObject = {};
+  //declare front of line holder variable
+  queueInstance.frontOfLine = 0;
+  //declare back of line of holder variable
+  queueInstance.backOfLine = 0;
+  //use extend technique from slide 25, but correct to README _extend
+  _.extend(queueInstance, queueMethods);
+  //return queueInstance, which is now a result of all operations
+  return queueInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  //need an enqueue method, which increments up back of line property
+  enqueue: function(value){
+    this.backOfLine ++;
+    this.storageObject[this.backOfLine] = value;
+  },
+
+  //need a dequeue method
+  dequeue: function(){
+    //test case for back larger than front
+    if (this.backOfLine > this.frontOfLine){
+      //increment up front holder variable
+      this.frontOfLine ++;
+      //declare variable for incremented front holder value
+      let tempFront = this.storageObject[this.frontOfLine];
+      //delete first value/key of front holder variable
+      delete this.storageObject[this.frontOfLine];
+      //return variable for incremented front holder value
+      return tempFront;
+    };
+  },
+
+  //return size for storage object
+  size: function(){
+    return (this.backOfLine - this.frontOfLine)
+  }
+
+};
 
 
