f67fc845ca71f0c6e0ce7f9411054fa3c156849c
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-03-05 13:27:50 -0600

    Completed prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,43 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // create instance object with stackMethods object as the prototype
+  let stackInstance = Object.create(stackMethods);
+  
+  // add storage object to stack instance
+  stackInstance.storage = {};
+
+  // add size property to stack instance
+  stackInstance.currentSize = 0;
+
+  // return stack instance
+  return stackInstance; 
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+// method for pushing values on to the stack
+push: function(value){
+  this.currentSize++;
+  this.storage[this.currentSize] = value;
+},
+
+// method for popping values off the stack
+pop: function(){
+  if (this.currentSize > 0){
+    let tempVal = this.storage[this.currentSize];
+    delete this.storage[this.currentSize];
+    this.currentSize--;
+    return tempVal;
+  }
+},
+
+// method for returning the size of the stack
+size: function(){
+  return this.currentSize;
+}
+
+};
 
 
